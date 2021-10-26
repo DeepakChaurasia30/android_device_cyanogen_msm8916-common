a6c8ddf6dfa051fb04aa28c66a6eb744dc79bef1
DeepakChaurasia30 <chaurasia.deepak.300@gmail.com>
2021-10-26 21:39:13 +0530

    Add logger service in dt.

--- msm8916.mk
+++ msm8916.mk
@@ -117,6 +117,10 @@ PRODUCT_PACKAGES += \
     init.recovery.qcom.rc \
     ueventd.qcom.rc
 
+# Logger  
+PRODUCT_PACKAGES += \
+    logger.rc    
+
 # IRQ balance
 ifeq ($(TARGET_BOARD_PLATFORM_VARIANT),msm8939)
 PRODUCT_COPY_FILES += \
